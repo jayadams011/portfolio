d8e75cb4f59122dd056f2e3541f22246615fed3a
andriiglukhyi <andrigluxii@gmail.com>
2018-06-11 14:50:43 -0700

    pdf resume

---
+++ bus_curd.py
@@ -0,0 +1,25 @@
+class SoftwareDeveloper():
+    def __init__(self):
+        self.email = 'AndriiGluxii@gmail.com'
+        self.phone = 9077382112
+        self.website = "andrii-glukhyi.com"
+        self.title = "Software Developer"
+        self.languages = ['Python', 'JavaScript']
+    
+    def email(self):
+        return self.email
+
+    def phone(self):
+        return self.phone
+    
+    def website(self):
+        return self.website
+    
+    def title(self):
+        return self.title
+    
+    def languages(self):
+        return self.languages
+
+
+        
